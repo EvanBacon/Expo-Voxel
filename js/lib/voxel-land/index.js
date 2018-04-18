b3e83692e550e43e59e651983d6901f158cf49ed
Evan Bacon <baconbrix@gmail.com>
2018-04-18 01:50:27 -0700

    updated format

--- js/lib/voxel-land/index.js
+++ js/lib/voxel-land/index.js
@@ -1,8 +1,7 @@
-
 var ndarray = require('ndarray');
 var EventEmitter = require('events').EventEmitter;
 var inherits = require('inherits');
-import ChunkGenerator from './worker'
+import ChunkGenerator from './worker';
 module.exports = function(game, opts) {
   return new Land(game, opts);
 };
@@ -14,10 +13,11 @@ module.exports.pluginInfo = {
 
 function Land(game, opts) {
   EventEmitter.call(this);
-  
+
   this.game = game;
 
-  if (!game.plugins || !game.plugins.get('voxel-registry')) throw new Error('voxel-land requires voxel-registry');
+  if (!game.plugins || !game.plugins.get('voxel-registry'))
+    throw new Error('voxel-land requires voxel-registry');
   this.registry = game.plugins.get('voxel-registry');
 
   opts = opts || {};
@@ -28,25 +28,27 @@ function Land(game, opts) {
   opts.crustUpper = opts.crustUpper === undefined ? 2 : opts.crustUpper;
   opts.hillScale = opts.hillScale || 20;
   opts.roughnessScale = opts.roughnessScale || 200;
-  opts.populateTrees = (opts.populateTrees !== undefined) ? opts.populateTrees : true;
+  opts.populateTrees =
+    opts.populateTrees !== undefined ? opts.populateTrees : true;
   opts.treesScale = opts.treesScale || 200;
-  opts.treesMaxDensity = (opts.treesMaxDensity !== undefined) ? opts.treesMaxDensity : 5;
+  opts.treesMaxDensity =
+    opts.treesMaxDensity !== undefined ? opts.treesMaxDensity : 5;
   opts.chunkSize = game.chunkSize || 32;
-  opts.chunkPad = game.chunkPad|0;
+  opts.chunkPad = game.chunkPad | 0;
 
-  opts.registerBlocks = opts.registerBlocks === undefined ? true : opts.registerBlocks;
-  opts.registerItems = opts.registerItems === undefined ? true : opts.registerItems;
-  opts.registerRecipes = opts.registerRecipes === undefined ? true : opts.registerRecipes;
+  opts.registerBlocks =
+    opts.registerBlocks === undefined ? true : opts.registerBlocks;
+  opts.registerItems =
+    opts.registerItems === undefined ? true : opts.registerItems;
+  opts.registerRecipes =
+    opts.registerRecipes === undefined ? true : opts.registerRecipes;
 
   // can't clone types, so need to send size instead
   if (game.arrayType === Uint8Array || game.arrayType === Uint8ClampedArray)
     opts.arrayElementSize = 1;
-  else if (game.arrayType === Uint16Array)
-    opts.arrayElementSize = 2;
-  else if (game.arrayType === Uint32Array)
-    opts.arrayElementSize = 4;
-  else
-    throw new Error('voxel-land unknown game.arrayType: ' + game.arrayType)
+  else if (game.arrayType === Uint16Array) opts.arrayElementSize = 2;
+  else if (game.arrayType === Uint32Array) opts.arrayElementSize = 4;
+  else throw new Error('voxel-land unknown game.arrayType: ' + game.arrayType);
 
   this.opts = JSON.parse(JSON.stringify(opts));
 
@@ -64,34 +66,91 @@ Land.prototype.disable = function() {
   // TODO: unregister blocks?
 };
 
-Land.prototype.registerBlocks = function()  {
+Land.prototype.registerBlocks = function() {
   if (this.opts.materials) return; // only register blocks once TODO: remove after adding unregister
 
   if (this.opts.registerItems) {
-    this.registry.registerItem('coal', {itemTexture: 'i/coal', fuelBurnTime: 1})
+    this.registry.registerItem('coal', {
+      itemTexture: 'i/coal',
+      fuelBurnTime: 1,
+    });
   }
 
   if (this.opts.registerBlocks) {
-    this.registry.registerBlock('grass', {texture: ['grass_top', 'dirt', 'grass_side'], hardness:1.0, itemDrop: 'dirt', effectiveTool: 'spade'});
-    this.registry.registerBlock('dirt', {texture: 'dirt', hardness:0.75, effectiveTool: 'spade'});
-    this.registry.registerBlock('stone', {displayName: 'Smooth Stone', texture: 'stone', hardness:10.0, itemDrop: 'cobblestone', effectiveTool: 'pickaxe', requiredTool: 'pickaxe'});
-    this.registry.registerBlock('logOak', {displayName: 'Oak Wood', texture: ['log_oak_top', 'log_oak_top', 'log_oak'], hardness:2.0, effectiveTool: 'axe', creativeTab: 'plants'});
-    this.registry.registerBlock('cobblestone', {texture: 'cobblestone', hardness:10.0, effectiveTool: 'pickaxe', requiredTool: 'pickaxe'});
-    this.registry.registerBlock('oreCoal', {displayName: 'Coal Ore', texture: 'coal_ore', itemDrop: 'coal', hardness:15.0, requiredTool: 'pickaxe'});
-    this.registry.registerBlock('oreIron', {displayName: 'Iron Ore', texture: 'iron_ore', hardness:15.0, requiredTool: 'pickaxe'});
-    this.registry.registerBlock('brick', {texture: 'brick'}); // some of the these blocks don't really belong here..do they?
-    this.registry.registerBlock('obsidian', {texture: 'obsidian', hardness: 128, requiredTool: 'pickaxe'});
-    this.registry.registerBlock('leavesOak', {displayName: 'Oak Leaves', texture: 'leaves_oak', transparent: true, hardness: 0.1, creativeTab: 'plants',
+    this.registry.registerBlock('grass', {
+      texture: ['grass_top', 'dirt', 'grass_side'],
+      hardness: 1.0,
+      itemDrop: 'dirt',
+      effectiveTool: 'spade',
+    });
+    this.registry.registerBlock('dirt', {
+      texture: 'dirt',
+      hardness: 0.75,
+      effectiveTool: 'spade',
+    });
+    this.registry.registerBlock('stone', {
+      displayName: 'Smooth Stone',
+      texture: 'stone',
+      hardness: 10.0,
+      itemDrop: 'cobblestone',
+      effectiveTool: 'pickaxe',
+      requiredTool: 'pickaxe',
+    });
+    this.registry.registerBlock('logOak', {
+      displayName: 'Oak Wood',
+      texture: ['log_oak_top', 'log_oak_top', 'log_oak'],
+      hardness: 2.0,
+      effectiveTool: 'axe',
+      creativeTab: 'plants',
+    });
+    this.registry.registerBlock('cobblestone', {
+      texture: 'cobblestone',
+      hardness: 10.0,
+      effectiveTool: 'pickaxe',
+      requiredTool: 'pickaxe',
+    });
+    this.registry.registerBlock('oreCoal', {
+      displayName: 'Coal Ore',
+      texture: 'coal_ore',
+      itemDrop: 'coal',
+      hardness: 15.0,
+      requiredTool: 'pickaxe',
+    });
+    this.registry.registerBlock('oreIron', {
+      displayName: 'Iron Ore',
+      texture: 'iron_ore',
+      hardness: 15.0,
+      requiredTool: 'pickaxe',
+    });
+    this.registry.registerBlock('brick', { texture: 'brick' }); // some of the these blocks don't really belong here..do they?
+    this.registry.registerBlock('obsidian', {
+      texture: 'obsidian',
+      hardness: 128,
+      requiredTool: 'pickaxe',
+    });
+    this.registry.registerBlock('leavesOak', {
+      displayName: 'Oak Leaves',
+      texture: 'leaves_oak',
+      transparent: true,
+      hardness: 0.1,
+      creativeTab: 'plants',
       // if voxel-food apple is enabled, drop it when breaking laves (oak apples)
-      itemDrop: this.registry.getItemProps('apple') ? 'apple' : null});
+      itemDrop: this.registry.getItemProps('apple') ? 'apple' : null,
+    });
 
-    this.registry.registerBlock('logBirch', {texture: ['log_birch_top', 'log_birch_top', 'log_birch'], hardness:2.0,
-      displayName: 'Birch Wood', effectiveTool: 'axe', creativeTab: 'plants'}); // TODO: generate
+    this.registry.registerBlock('logBirch', {
+      texture: ['log_birch_top', 'log_birch_top', 'log_birch'],
+      hardness: 2.0,
+      displayName: 'Birch Wood',
+      effectiveTool: 'axe',
+      creativeTab: 'plants',
+    }); // TODO: generate
   }
 
   if (this.opts.registerRecipes) {
     var recipes = this.game.plugins.get('voxel-recipes');
-    if (recipes) { // TODO: should these be properties on voxel-registry, instead?
+    if (recipes) {
+      // TODO: should these be properties on voxel-registry, instead?
       recipes.thesaurus.registerName('wood.log', 'logOak');
       recipes.thesaurus.registerName('wood.log', 'logBirch');
       recipes.thesaurus.registerName('tree.leaves', 'leavesOak');
@@ -102,7 +161,11 @@ Land.prototype.registerBlocks = function()  {
 
   if (!this.opts.materials) {
     this.opts.materials = {};
-    for (var blockIndex = 1; blockIndex < this.registry.blockProps.length; blockIndex += 1) {
+    for (
+      var blockIndex = 1;
+      blockIndex < this.registry.blockProps.length;
+      blockIndex += 1
+    ) {
       var name = this.registry.getBlockName(blockIndex);
       var packedIndex = this.registry.getBlockIndex(name);
       this.opts.materials[name] = packedIndex;
@@ -113,53 +176,55 @@ Land.prototype.registerBlocks = function()  {
 Land.prototype.bindEvents = function() {
   var self = this;
 
-
   let chunkGen = new ChunkGenerator(this, self.opts);
   // self.emit({cmd: 'configure', opts:self.opts});
 
-  this.game.voxels.emitter.addListener('missingChunk', this.missingChunk = function(pos) {
-// console.warn("packitnsionfmd")
-    // self.emit({cmd: 'generateChunk', pos:pos})
-    if (chunkGen === undefined) throw new Error('voxel-land web worker error: received "generateChunk" before "configure"');
-    const _chunk = chunkGen.generateChunk(pos);
+  this.game.voxels.emitter.addListener(
+    'missingChunk',
+    (this.missingChunk = function(pos) {
+      // console.warn("packitnsionfmd")
+      // self.emit({cmd: 'generateChunk', pos:pos})
+      if (chunkGen === undefined)
+        throw new Error(
+          'voxel-land web worker error: received "generateChunk" before "configure"',
+        );
+      const _chunk = chunkGen.generateChunk(pos);
 
-    var voxels = new self.game.arrayType(_chunk.voxelBuffer);
-    // var chunk = ndarray(voxels, [self.opts.chunkSize+self.opts.chunkPad, self.opts.chunkSize+self.opts.chunkPad, self.opts.chunkSize+self.opts.chunkPad]);
-    let chunk = {}
-    const chunkSize = 32;
-    chunk.position = _chunk.position;
-    chunk.dims =  [chunkSize, chunkSize, chunkSize]
-    chunk.voxels = voxels;
-    self.game.showChunk(chunk);
+      var voxels = new self.game.arrayType(_chunk.voxelBuffer);
+      // var chunk = ndarray(voxels, [self.opts.chunkSize+self.opts.chunkPad, self.opts.chunkSize+self.opts.chunkPad, self.opts.chunkSize+self.opts.chunkPad]);
+      let chunk = {};
+      const chunkSize = 32;
+      chunk.position = _chunk.position;
+      chunk.dims = [chunkSize, chunkSize, chunkSize];
+      chunk.voxels = voxels;
+      self.game.showChunk(chunk);
 
-    var {changes} = _chunk;
-    if (changes) {
-      
-      for (var i = 0; i < changes.length; ++i) {
-        var pos = changes[i][0];
-        var value = changes[i][1];
+      var { changes } = _chunk;
+      if (changes) {
+        for (var i = 0; i < changes.length; ++i) {
+          var pos = changes[i][0];
+          var value = changes[i][1];
 
-        self.game.setBlock(pos, value); // TODO: faster mass edit?
-        // TODO: what if pos is out of loaded chunk range? doesn't automatically load chunk; change will be lost
+          self.game.setBlock(pos, value); // TODO: faster mass edit?
+          // TODO: what if pos is out of loaded chunk range? doesn't automatically load chunk; change will be lost
+        }
+      } else {
       }
 
-    } else {
-
-    }
-
-    // var voxels = generateChunk(p, chunkSize)
-    // var chunk = {
-    //   position: p,
-    //   dims: [chunkSize, chunkSize, chunkSize],
-    //   voxels: voxels
-    // }
-    // game.showChunk(chunk)
-  });
+      // var voxels = generateChunk(p, chunkSize)
+      // var chunk = {
+      //   position: p,
+      //   dims: [chunkSize, chunkSize, chunkSize],
+      //   voxels: voxels
+      // }
+      // game.showChunk(chunk)
+    }),
+  );
 
   // self.addListener('message', function(ev) {
   //   console.warn("thrown back", ev.data.cmd);
   //   if (ev.data.cmd === 'chunkGenerated') {
-   
+
   //   } else if (ev.data.cmd === 'decorate') {
   //     var changes = ev.data.changes;
   //     for (var i = 0; i < changes.length; ++i) {
--- js/lib/voxel/index.js
+++ js/lib/voxel/index.js
@@ -1,114 +1,142 @@
-var chunker = require('./chunker')
-var ndarray = require('ndarray')
-
+var chunker = require('./chunker');
+var ndarray = require('ndarray');
 
 module.exports = function(opts) {
-  if (!opts.generateVoxelChunk) opts.generateVoxelChunk = function(low, high) {
-    return _generate(low, high, module.exports.generator['Valley'])
-  }
-  return chunker(opts)
-}
+  if (!opts.generateVoxelChunk)
+    opts.generateVoxelChunk = function(low, high) {
+      return _generate(low, high, module.exports.generator['Valley']);
+    };
+  return chunker(opts);
+};
 
 module.exports.meshers = {
   culled: require('./meshers/culled').mesher,
   greedy: require('./meshers/greedy').mesher,
   transgreedy: require('./meshers/transgreedy').mesher,
   monotone: require('./meshers/monotone').mesher,
-  stupid: require('./meshers/stupid').mesher
-}
+  stupid: require('./meshers/stupid').mesher,
+};
 
-module.exports.Chunker = chunker.Chunker
-module.exports.geometry = {}
-module.exports.generator = {}
-module.exports._generate = _generate
-module.exports.generate = generate
+module.exports.Chunker = chunker.Chunker;
+module.exports.geometry = {};
+module.exports.generator = {};
+module.exports._generate = _generate;
+module.exports.generate = generate;
 // from https://github.com/mikolalysenko/mikolalysenko.github.com/blob/master/MinecraftMeshes2/js/testdata.js#L4
 function _generate(l, h, f) {
-  var d = [ h[0]-l[0], h[1]-l[1], h[2]-l[2] ]
-    , v = new Int8Array(d[0]*d[1]*d[2])
-    , n = 0;
-  for(var k=l[2]; k<h[2]; ++k)
-  for(var j=l[1]; j<h[1]; ++j)
-  for(var i=l[0]; i<h[0]; ++i, ++n) {
-    v[n] = f(i,j,k);
-  }
-  return {voxels:v, dims:d};
+  var d = [h[0] - l[0], h[1] - l[1], h[2] - l[2]],
+    v = new Int8Array(d[0] * d[1] * d[2]),
+    n = 0;
+  for (var k = l[2]; k < h[2]; ++k)
+    for (var j = l[1]; j < h[1]; ++j)
+      for (var i = l[0]; i < h[0]; ++i, ++n) {
+        v[n] = f(i, j, k);
+      }
+  return { voxels: v, dims: d };
 }
 
 function generate(lo, hi, fn) {
   // To fix the display gaps, we need to pad the bounds
-  lo[0]--
-  lo[1]--
-  lo[2]--
-  hi[0]++
-  hi[1]++
-  hi[2]++
-  var dims = [hi[2]-lo[2], hi[1]-lo[1], hi[0]-lo[0]]
-  var data = ndarray(new Uint16Array(dims[2] * dims[1] * dims[0]), dims)
+  lo[0]--;
+  lo[1]--;
+  lo[2]--;
+  hi[0]++;
+  hi[1]++;
+  hi[2]++;
+  var dims = [hi[2] - lo[2], hi[1] - lo[1], hi[0] - lo[0]];
+  var data = ndarray(new Uint16Array(dims[2] * dims[1] * dims[0]), dims);
   for (var k = lo[2]; k < hi[2]; k++)
     for (var j = lo[1]; j < hi[1]; j++)
-      for(var i = lo[0]; i < hi[0]; i++) {
-        data.set(k-lo[2], j-lo[1], i-lo[0], fn(i, j, k))
+      for (var i = lo[0]; i < hi[0]; i++) {
+        data.set(k - lo[2], j - lo[1], i - lo[0], fn(i, j, k));
       }
-  return data
+  return data;
 }
 
 // shape and terrain generator functions
-module.exports.generator['Sphere'] = function(i,j,k) {
-  return i*i+j*j+k*k <= 16*16 ? 1 : 0
-}
+module.exports.generator['Sphere'] = function(i, j, k) {
+  return i * i + j * j + k * k <= 16 * 16 ? 1 : 0;
+};
 
-module.exports.generator['Noise'] = function(i,j,k) {
+module.exports.generator['Noise'] = function(i, j, k) {
   return Math.random() < 0.1 ? Math.random() * 0xffffff : 0;
-}
+};
 
-module.exports.generator['Dense Noise'] = function(i,j,k) {
+module.exports.generator['Dense Noise'] = function(i, j, k) {
   return Math.round(Math.random() * 0xffffff);
-}
+};
 
-module.exports.generator['Checker'] = function(i,j,k) {
-  return !!((i+j+k)&1) ? (((i^j^k)&2) ? 1 : 0xffffff) : 0;
-}
+module.exports.generator['Checker'] = function(i, j, k) {
+  return !!((i + j + k) & 1) ? ((i ^ j ^ k) & 2 ? 1 : 0xffffff) : 0;
+};
 
-module.exports.generator['Hill'] = function(i,j,k) {
-  return j <= 16 * Math.exp(-(i*i + k*k) / 64) ? 1 : 0;
-}
+module.exports.generator['Hill'] = function(i, j, k) {
+  return j <= 16 * Math.exp(-(i * i + k * k) / 64) ? 1 : 0;
+};
 
-module.exports.generator['Valley'] = function(i,j,k) {
-  return j <= (i*i + k*k) * 31 / (32*32*2) + 1 ? 1 : 0;
-}
+module.exports.generator['Valley'] = function(i, j, k) {
+  return j <= (i * i + k * k) * 31 / (32 * 32 * 2) + 1 ? 1 : 0;
+};
 
-module.exports.generator['Hilly Terrain'] = function(i,j,k) {
+module.exports.generator['Hilly Terrain'] = function(i, j, k) {
   var h0 = 3.0 * Math.sin(Math.PI * i / 12.0 - Math.PI * k * 0.1) + 27;
-  if(j > h0+1) {
+  if (j > h0 + 1) {
     return 0;
   }
-  if(h0 <= j) {
+  if (h0 <= j) {
     return 1;
   }
   var h1 = 2.0 * Math.sin(Math.PI * i * 0.25 - Math.PI * k * 0.3) + 20;
-  if(h1 <= j) {
+  if (h1 <= j) {
     return 2;
   }
-  if(2 < j) {
+  if (2 < j) {
     return Math.random() < 0.1 ? 0x222222 : 0xaaaaaa;
   }
   return 3;
-}
+};
 
-module.exports.scale = function ( x, fromLow, fromHigh, toLow, toHigh ) {
-  return ( x - fromLow ) * ( toHigh - toLow ) / ( fromHigh - fromLow ) + toLow
-}
+module.exports.scale = function(x, fromLow, fromHigh, toLow, toHigh) {
+  return (x - fromLow) * (toHigh - toLow) / (fromHigh - fromLow) + toLow;
+};
 
 // convenience function that uses the above functions to prebake some simple voxel geometries
 module.exports.generateExamples = function() {
   return {
-    'Sphere': _generate([-16,-16,-16], [16,16,16], module.exports.generator['Sphere']),
-    'Noise': _generate([0,0,0], [16,16,16], module.exports.generator['Noise']),
-    'Dense Noise': _generate([0,0,0], [16,16,16], module.exports.generator['Dense Noise']),
-    'Checker': _generate([0,0,0], [8,8,8], module.exports.generator['Checker']),
-    'Hill': _generate([-16, 0, -16], [16,16,16], module.exports.generator['Hill']),
-    'Valley': _generate([0,0,0], [32,32,32], module.exports.generator['Valley']),
-    'Hilly Terrain': _generate([0, 0, 0], [32,32,32], module.exports.generator['Hilly Terrain'])
-  }
-}
+    Sphere: _generate(
+      [-16, -16, -16],
+      [16, 16, 16],
+      module.exports.generator['Sphere'],
+    ),
+    Noise: _generate(
+      [0, 0, 0],
+      [16, 16, 16],
+      module.exports.generator['Noise'],
+    ),
+    'Dense Noise': _generate(
+      [0, 0, 0],
+      [16, 16, 16],
+      module.exports.generator['Dense Noise'],
+    ),
+    Checker: _generate(
+      [0, 0, 0],
+      [8, 8, 8],
+      module.exports.generator['Checker'],
+    ),
+    Hill: _generate(
+      [-16, 0, -16],
+      [16, 16, 16],
+      module.exports.generator['Hill'],
+    ),
+    Valley: _generate(
+      [0, 0, 0],
+      [32, 32, 32],
+      module.exports.generator['Valley'],
+    ),
+    'Hilly Terrain': _generate(
+      [0, 0, 0],
+      [32, 32, 32],
+      module.exports.generator['Hilly Terrain'],
+    ),
+  };
+};
